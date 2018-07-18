c24980345f8af905ee395f2f1eaef21317e4d3ad
wangxinwei516030910041 <1351232837@qq.com>
2018-07-18 10:46:13 +0800

    record activity 框架

---
+++ app/src/main/java/com/sjtubus/model/RecordInfo.java
@@ -0,0 +1,18 @@
+package com.sjtubus.model;
+
+/**
+ * @author wxw
+ * @date 2018/7/18 10:09
+ * */
+
+public class RecordInfo {
+
+    private String confirm_date; //提交预约申请的时间
+
+    private String departure_date;
+    private String departure_time; //预约的日期和时间
+
+    private String line_name;
+    private String shiftid;
+
+}
